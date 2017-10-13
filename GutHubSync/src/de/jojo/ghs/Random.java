370d7d91606d85e7ea1eca57abe7df31a91d5964
kaetzacoatl <>
2017-10-14 01:27:48 +0200

    Revert "and again"
    This reverts commit 7e7e3fe4892a28c27d4b094047af4658d06cc4d3.

--- GutHubSync/src/de/jojo/ghs/Random.java
+++ GutHubSync/src/de/jojo/ghs/Random.java
@@ -5,10 +5,7 @@ public class Random {
 	public static void main(String[] args) {
 		System.out.println(SomeOtherClass.onlineMethod(5, 7));
 		System.out.println("Hello world!");
-<<<<<<< HEAD
 		//...end a little bit here
-=======
->>>>>>> dba5d666c06dc6838db396e392e6fb3385d18268
 	}
 
 }
