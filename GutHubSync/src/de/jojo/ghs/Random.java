931e0025d1cce5e0ae6d852c494cd862bef044db
kaetzacoatl <kaetzacoatl@users.noreply.github.com>
2017-10-14 01:19:58 +0200

    break test

--- GutHubSync/src/de/jojo/ghs/Random.java
+++ GutHubSync/src/de/jojo/ghs/Random.java
@@ -5,6 +5,7 @@ public class Random {
 	public static void main(String[] args) {
 		System.out.println(SomeOtherClass.onlineMethod(5, 7));
 		System.out.println("Hello world!");
+		//let this brake it
 	}
 
 }
